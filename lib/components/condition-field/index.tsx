af38542a46719f62e784b293df7d9800ce7d3f36
mihanizm56 <m727507.56@mail.ru>
2020-11-01 00:46:10 +0300

    fix(scripts): fix ConditionField

--- lib/components/condition-field/index.tsx
+++ lib/components/condition-field/index.tsx
@@ -1,15 +1,18 @@
-import React, { PropsWithChildren } from 'react';
+import React, { memo, PropsWithChildren } from 'react';
 import { Field } from 'react-final-form';
 import classnames from 'classnames/bind';
 import { CSSTransition } from 'react-transition-group';
+import { FormApi } from 'final-form';
 import { isValueSelected } from './utils/is-value-selected';
 import styles from './index.module.scss';
 
 type PropsType = PropsWithChildren<{
+  fieldNameToTrack: string;
   fieldName: string;
   isAnimated?: boolean;
   fieldValue?: any;
   customCondition?: ({ value, name }: { value: any; name: string }) => boolean;
+  form: FormApi<any>;
 }>;
 
 const BLOCK_NAME = 'Animated-field';
@@ -17,36 +20,44 @@ const cn = classnames.bind(styles);
 
 const TIMEOUT_OPEN = 300;
 
-export const ConditionField = ({
-  fieldName,
-  fieldValue,
-  children,
-  customCondition,
-  isAnimated,
-}: PropsType) => (
-  <Field name={fieldName} subscription={{ value: true }}>
-    {({ input: { value } }) => {
-      const isSelected = customCondition
-        ? customCondition({ value, name: fieldName })
-        : isValueSelected({ selectedValue: fieldValue, fieldValue: value });
+export const ConditionField = memo(
+  ({
+    fieldValue,
+    fieldName,
+    children,
+    customCondition,
+    isAnimated,
+    form,
+    fieldNameToTrack,
+  }: PropsType) => (
+    <Field name={fieldNameToTrack} subscription={{ value: true }}>
+      {({ input: { value } }) => {
+        const isSelected = customCondition
+          ? customCondition({ value, name: fieldNameToTrack })
+          : isValueSelected({ selectedValue: fieldValue, fieldValue: value });
 
-      if (isAnimated) {
-        return (
-          <CSSTransition
-            classNames={{
-              enter: cn(`${BLOCK_NAME}--enter`),
-              exit: cn(`${BLOCK_NAME}--exit`),
-            }}
-            in={isSelected}
-            timeout={TIMEOUT_OPEN}
-            unmountOnExit
-          >
-            {children}
-          </CSSTransition>
-        );
-      }
+        if (!isSelected) {
+          form.change(fieldName, undefined);
+        }
 
-      return isSelected ? children : null;
-    }}
-  </Field>
+        if (isAnimated) {
+          return (
+            <CSSTransition
+              classNames={{
+                enter: cn(`${BLOCK_NAME}--enter`),
+                exit: cn(`${BLOCK_NAME}--exit`),
+              }}
+              in={isSelected}
+              timeout={TIMEOUT_OPEN}
+              unmountOnExit
+            >
+              {children}
+            </CSSTransition>
+          );
+        }
+
+        return isSelected ? children : null;
+      }}
+    </Field>
+  ),
 );
